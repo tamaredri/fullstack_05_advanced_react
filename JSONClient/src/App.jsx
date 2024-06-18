088b6246a72ad62e6048d35d00703ab8bc22c56a
tamar edri <tedri@g.jct.ac.il>
2024-06-18 08:28:43 +0300

    added to git ignore

--- JSONClient/src/App.jsx
+++ JSONClient/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  fetch('http://localhost:3000/posts')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
   return (
     <>
       
