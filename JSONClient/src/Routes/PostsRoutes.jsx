550e0bfb712208e40dd53c2e1c60cfd54f228552
איילת אברהם <iyeletav2@gmail.com>
2024-06-24 07:56:55 +0300

    merege postRoutes

--- JSONClient/src/Routes/PostsRoutes.jsx
+++ JSONClient/src/Routes/PostsRoutes.jsx
@@ -28,36 +28,3 @@ const PostsRoutes = () => {
 };
 
 export default PostsRoutes;
-
-/*import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import ListPosts from '../Components/PostsComponents/ListPosts';
-import NewPost from '../Components/PostsComponents/NewPost';
-import SinglePost from '../Components/PostsComponents/SinglePost';
-
-const PostsRoutes = () => {
-    return (
-      <Routes>
-        <Route path="/" element={<ListPosts />} />
-        <Route path="/new" element={<NewPost />} />
-        <Route path="/:id" element={<SinglePost />} />
-      </Routes>
-    );
-};
-
-export default PostsRoutes;
-
-/*import React from 'react';
-import ListPosts from '../Components/ListPosts'
-import NewPost from '../Components/NewPost'
-
-const PostsRoutes = () => {
-    return (
-      <Routes>
-        <Route path="/" element={<ListPosts />} />
-        <Route path="/new" element={<NewPost />} />
-      </Routes>
-    );
-  };
-
-export default PostsRoutes;*/
